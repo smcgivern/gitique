48daaa849dd6acfbc23ad70609b7d6914ae95241
Sean McGivern <sean@mcgivern.me.uk>
2015-10-08 21:03:14 +0100

    Add source maps in dev build

--- script/dev.clj
+++ script/dev.clj
@@ -1,3 +1,6 @@
 (require 'cljs.closure)
 
-(cljs.closure/watch "src" {:optimizations :whitespace :output-to "gitique.js"})
+(cljs.closure/watch "src" {:optimizations :whitespace
+                           :output-to "gitique.js"
+                           :output-dir "out"
+                           :source-map "gitique.js.map"})
